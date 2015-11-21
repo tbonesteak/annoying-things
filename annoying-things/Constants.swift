0a95d17508c97f69e4301deea7742ea7cc2f917b
Jon <User@Macs-MacBook-Pro.local>
2015-11-21 00:48:05 -0600

    Working on the UI of the login screen

---
+++ annoying-things/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  annoying-things
+//
+//  Created by Jon on 11/20/15.
+//  Copyright © 2015 Coder Vox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//We don't put constants into a class because that gives it a limited scope. We want our constants globally accessible.
+
+
+//This is to give our view a shadow color.
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ annoying-things/MaterialButton.swift
@@ -0,0 +1,22 @@
+//
+//  MaterialButton.swift
+//  annoying-things
+//
+//  Created by Jon on 11/21/15.
+//  Copyright © 2015 Coder Vox. All rights reserved.
+//
+
+import UIKit
+
+class MaterialButton: UIButton {
+
+    //awakeFromNib is called when the user interface is loaded from the storyboard.
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0 //How much blur and shadow coverage you want
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+
+}

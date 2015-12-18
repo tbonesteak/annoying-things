6d9183c211dcd9c4cbb6fc3abc1685461ad45ebe
Jon <User@Macs-MacBook-Pro.local>
2015-12-18 02:07:57 -0600

    made post upload images to server

--- annoying-things/FeedVC.swift
+++ annoying-things/FeedVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
@@ -134,7 +135,69 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     }
     
     @IBAction func makePost(sender: AnyObject) {
+        
+        //Making sure the description field is not empty.
+        if let txt = postField.text where txt != "" {
+            
+            //Checking to see if there is an image
+            if let img = imageSelectorImage.image {
+                
+                //This is imageshack endpoint URL that we need to upload to.
+                let urlStr = "https://post.imageshack.us/upload_api.php"
+                //Taking the link and turning it into an NSURL.
+                let url = NSURL(string: urlStr)!
+                //Converting the image to data amd as a JPEG, and also compressing it. 0 means totally compressed, 1 means fully uncompressed.
+                let imgData = UIImageJPEGRepresentation(img, 0.2)!
+                //Imageshack API key, and converting it to data.
+                let keyData = "P3Q08ZW98ea1d510dde3c37d688cdc0bd832ac05".dataUsingEncoding(NSUTF8StringEncoding)!
+                //Converting the word JSON into data because it's a required parameter.
+                let keyJSON = "json".dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                //We need to do a multiform data request because we are uploading several different things and of different file types (namely, the image file and the API key and format. So, we need to convert all of it to NSData.
+                //In Alamofire, there is a method MultipartFormData, and this is what we are using for that purpose.
+                //We're saying it's a POST request, it wants a URL so we're giving it that.
+                //You can command click on upload to see what the requirements are.
+                Alamofire.upload(.POST, url, multipartFormData: { multipartFormData in
+                    //In this closure, we need to add the fields that the POST request is gonna require.
+                    
+                    //Passing in the image data and giving it the name "fileupload" as required from the API doc. fileName can be anything, so we're just calling it image for now, but Imageshack will rename that automatically. mimeType is the type of image we are transferring.
+                    multipartFormData.appendBodyPart(data: imgData, name: "fileupload", fileName:"image", mimeType: "image/jpg")
+                    //The API key. The name is "key" as required from the API doc.
+                    multipartFormData.appendBodyPart(data: keyData, name: "key")
+                    multipartFormData.appendBodyPart(data: keyJSON, name: "format")
+                    
+                    }) { encodingResult in
+                        //After the POST request is made, the result is going to come through here. It will come back with either success or an error.
+                        //We're doing a swtich statement in here
+                        
+                        //.Success and .Failure are part of Alamofire's multipartformdata.
+                        switch encodingResult {
+                        //If it was successful, we're grabbing the JSON it sends back
+                        case .Success(let upload, _, _):
+                            upload.responseJSON(completionHandler: { request, response, result in
+                                
+                                //We want to grab the image link out of the JSON. This is a dictionary within a dictionary.
+                                //We're taking the value of the result we got back and converting it to a dictionary with keys of type string and values of type AnyObject.
+                                if let info = result.value as? Dictionary<String, AnyObject> {
+                                    
+                                    //Grabbing the links dictionary that is contained within.
+                                    if let links = info["links"] as? Dictionary<String, AnyObject> {
+                                        //Now grabbing the actual image link.
+                                        if let imgLink = links["image_link"] as? String {
+                                            print("LINK: \(imgLink)")
+                                        }
+                                    }
+                                }
+                                
+                            })
+                        //If it fails, let's just print the error.
+                        case .Failure(let error):
+                            print (error)
+                        }
+            }
+            
+        }
     }
-    
+   }
     
 }
